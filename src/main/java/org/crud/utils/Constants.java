1809f3c89b493649bada1e9ff0d3a80d9f57c006
rafael <rafaelmagalhaes1@live.com>
2024-06-09 20:57:25 -0300

    feat: Constants class created

---
+++ src/main/java/org/crud/utils/Constants.java
@@ -0,0 +1,21 @@
+package org.crud.utils;
+
+/**
+ * The type Constants.
+ */
+public class Constants {
+
+  /**
+   * Constants
+   */
+  public static final String FILE_PATH = "data.xml";
+  public static final String ELEMENT_HEADER_NAME = "record";
+  public static final String ERROR_LOADING_XML = "Error loading XML file";
+  public static final String ERROR_CREATING_RECORD = "Error creating record: ";
+  public static final String ERROR_UPDATING_RECORD = "Error updating record: ";
+  public static final String ERROR_DELETING_RECORD = "Error deleting record: ";
+  public static final String RECORD_CREATED_SUCCESS = "Record created successfully";
+  public static final String RECORD_UPDATED_SUCCESS = "Record updated successfully";
+  public static final String RECORD_DELETED_SUCCESS = "Record deleted successfully";
+  public static final String RECORD_NOT_FOUND = "Record not found";
+}

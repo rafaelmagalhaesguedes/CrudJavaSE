9e5c225680cbd3894b099ee612c18613c477323b
rafael <rafaelmagalhaes1@live.com>
2024-06-10 12:25:34 -0300

    feat: Constants class created

--- src/main/java/org/crud/utils/Constants.java
+++ src/main/java/org/crud/utils/Constants.java
@@ -8,14 +8,27 @@ public class Constants {
   /**
    * Constants
    */
-  public static final String FILE_PATH = "data.xml";
-  public static final String ELEMENT_HEADER_NAME = "record";
+  public static final String FILE_PATH = "src/main/resources/database.xml";
   public static final String ERROR_LOADING_XML = "Error loading XML file";
+
+  public static final String GET = "GET";
+  public static final String POST = "POST";
+  public static final String PUT = "PUT";
+  public static final String DELETE = "DELETE";
+  public static final String METHOD_NOT_ALLOWED = "Method Not Allowed";
+
+  public static final String ELEMENT_TAG_HEADER = "record";
+  public static final String ELEMENT_TAG_ID = "id";
+
   public static final String ERROR_CREATING_RECORD = "Error creating record: ";
   public static final String ERROR_UPDATING_RECORD = "Error updating record: ";
   public static final String ERROR_DELETING_RECORD = "Error deleting record: ";
+  public static final String RECORD_NOT_FOUND = "Record not found";
+
   public static final String RECORD_CREATED_SUCCESS = "Record created successfully";
   public static final String RECORD_UPDATED_SUCCESS = "Record updated successfully";
   public static final String RECORD_DELETED_SUCCESS = "Record deleted successfully";
-  public static final String RECORD_NOT_FOUND = "Record not found";
+
+  public static final String INTERNAL_ERROR = "Internal Error, try again.";
+  public static final String INVALID_DATA = "Invalid data";
 }
